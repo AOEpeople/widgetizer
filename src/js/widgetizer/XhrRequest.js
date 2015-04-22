3da3210fe4b10f879503ed01ac4ef1895c855e00
Marc Dix <marc.dix@aoe.com>
2015-04-22 12:29:03 +0200

    Explicitly defining the content-type because our demo nginx does not set a content-type at all

--- src/js/widgetizer/XhrRequest.js
+++ src/js/widgetizer/XhrRequest.js
@@ -6,7 +6,7 @@ AOEWidgetizer.XHRRequest = function() {
             //@TODO: Think about the qwest bonus feature to limit requests via qwest.limit(4); If such things
             // are not needed we could switch to marmottajax which is way smaller but provides
             // the promises we need (check ie8 support then!)
-            return qwest.get(requestUri);
+            return qwest.get(requestUri, null, {responseType: 'json'});
         }
     };
 };
--- src/spec/Widgetizer/Unit/XhrRequestSpec.js
+++ src/spec/Widgetizer/Unit/XhrRequestSpec.js
@@ -6,8 +6,8 @@ describe("AOEWidgetizer.XHRRequest", function () {
     });
 
     it('can make a request and get a valid json', function(done) {
-       xhrRequester.getWidgetJSON('http://localhost:8000/mock/19').then(function(markup) {
-           expect(markup).toMatch(/Hennessy VSOP Privilege Limited Edition/);
+       xhrRequester.getWidgetJSON('http://localhost:8000/mock/19').then(function(jsonResponse) {
+           expect(jsonResponse.collection.items[0].data.title).toBe('Hennessy VSOP Privilege Limited Edition 40% 1L, Geschenkverpackung (HM)');
            done();
        });
     });
