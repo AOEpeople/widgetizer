d59ab600fca13fd7be5059f01e712a45f7a5f680
Marc Dix <marc.dix@aoe.com>
2016-05-11 13:32:01 +0200

    added uglify and jshint

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,8 @@ var karmaServer = require('karma').Server;
 var wrap        = require('gulp-wrap');
 var server      = require('./mock/server');
 var browserify  = require('browserify');
-var source      = require('vinyl-source-stream');
+var vinylSource = require('vinyl-source-stream');
+var vinylBuffer = require('vinyl-buffer');
 var mockServer;
 
 var paths = {};
@@ -26,15 +27,24 @@ gulp.task('copy', ['clean'], function() {
         .pipe(gulp.dest(paths.dist));
 });
 
-gulp.task('widgetizer', ['clean', 'test', 'copy'], function() {
+gulp.task('widgetizer', ['clean', 'test', 'copy', 'jshint'], function() {
     return browserify('./src/js/WidgetizerGlobalAndAMD.js')
         .bundle()
         .on('error', function (err) {
             console.error(err);
             this.emit('end');
         })
-        .pipe(source('widgetizer_standalone.min.js'))
+        .pipe(vinylSource('widgetizer_standalone.min.js'))
+        .pipe(vinylBuffer())
+        .pipe(uglify())
         .pipe(gulp.dest(paths.dist));
+});
+
+gulp.task('jshint', function() {
+     return gulp.src(['./src/js/**/**.js'])
+         .pipe(jshint())
+         .pipe(jshint.reporter('jshint-stylish'))
+         .pipe(jshint.reporter('fail'))
 });
 
 gulp.task('start_mock_server', function() {
@@ -51,7 +61,7 @@ gulp.task('stop_mock_server', function() {
 });
 
 // PRIVATE TASK, RUN 'test' instead
-gulp.task('_unittest', ['start_mock_server'], function() {
+gulp.task('_unittest', ['start_mock_server', 'jshint'], function() {
     return new karmaServer({
         configFile: __dirname + '/karma.conf.js',
         singleRun: true,
@@ -75,5 +85,4 @@ gulp.task('_componenttest', ['_unittest'], function() {
 });
 
 gulp.task('test', ['_unittest', '_componenttest']);
-gulp.task('build', ['widgetizer'/*, 'widgetizer_jquery'*/]);
-gulp.task('default', ['build']);
+gulp.task('default', ['widgetizer']);
