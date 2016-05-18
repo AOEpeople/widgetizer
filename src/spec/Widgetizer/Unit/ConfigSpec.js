5c53bf9f9b1e82e6289298a3014d08c5c18daa8b
Marc Dix <marc.dix@aoe.com>
2016-05-18 10:56:44 +0200

    Removed unused config files

--- src/spec/Widgetizer/Unit/ConfigSpec.js
+++
@@ -1,6 +0,0 @@
-describe("config", function () {
-    it('self initializes and returns an object', function() {
-        expect(AOEWidgetizer.config).not.toBeNull();
-        expect(typeof AOEWidgetizer.config).toBe('object');
-    });
-});
